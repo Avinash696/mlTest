ebfa6a4ee20331abc864ed761e99d4d1f2c054e7
avinash696 <avinash696@gmail.com>
2021-08-04 14:20:33 +0530

    Ml kit

---
+++ app/src/main/java/com/tia/mltest/MainActivity2.java
@@ -0,0 +1,53 @@
+package com.tia.mltest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.PointF;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
+
+public class MainActivity2 extends AppCompatActivity  implements QRCodeReaderView.OnQRCodeReadListener {
+    private TextView resultTextView;
+    private QRCodeReaderView qrCodeReaderView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        resultTextView=findViewById(R.id.result_text_view);
+        qrCodeReaderView = (QRCodeReaderView) findViewById(R.id.qrdecoderview);
+        qrCodeReaderView.setOnQRCodeReadListener(this);
+
+        // Use this function to enable/disable decoding
+        qrCodeReaderView.setQRDecodingEnabled(true);
+
+        // Use this function to change the autofocus interval (default is 5 secs)
+        qrCodeReaderView.setAutofocusInterval(2000L);
+
+        // Use this function to enable/disable Torch
+        qrCodeReaderView.setTorchEnabled(true);
+
+        // Use this function to set front camera preview
+        qrCodeReaderView.setFrontCamera();
+
+        // Use this function to set back camera preview
+        qrCodeReaderView.setBackCamera();
+    }
+
+    @Override
+    public void onQRCodeRead(String text, PointF[] points) {
+        resultTextView.setText(text);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        qrCodeReaderView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        qrCodeReaderView.stopCamera();
+    }
+}
